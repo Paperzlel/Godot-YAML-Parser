267f0903ed6d00dd650d3a5b38df874976b91225
Paperzlel <lpitchfork@hotmail.com>
2024-07-11 13:53:47 +0100

    Re-wrote to create a better system
    The depth now works recursively rather than manually, and is much easier to read. Comments have also been added to increase readability

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Godot YAML Parser
  A GDScript YAML parser for Godot Engine
 
-## Usage
-Simply paste the script into your Godot project and use `YAMLParser.new(filepath)` to create an instance of it in your code. The filepath should be in the format `path/to/folder/`, do NOT add the `.yaml` ending to the path as it will not work correctly and the parser will break.
 
-To parse a file, use `YAMLParser.get_and_parse_yaml_file(name)`. This will look for a file under the name given. Do NOT give it the `.yaml` file ending, as that has already been accounted for in the filepath.
-The returned file will be an undindented dictionary, so you can save it using Godot's own JSON resource. The parser does not currently support converting dictionaries into YAML files, so be aware.
+## How to Use
+Using the parser is very simple, all you need to do is to download the `yaml_parser.gd` file either from source or from the Releases page, paste it into a folder in your project, create a new instance of the class using the `.new()` method, and call `get_and_parse_yaml_file()` to recieve the dictionary.
+The `yaml_dict` property only contains one dictionary per use, and will be overwritten if you use it more than once per file. If one wishes to use said dictionaries more than once, try assigning them to an array of dictionaries that one can access without worry of them being overwritten.
 
-Once the file has been parsed, it is saved to `YAMLParser.yaml_dict`, which you can re-use to prevent repetitive re-parsing. 
+## Features
+The current set of features are relatively simple: parse a YAML file into a Dictionary, and give to the user. As I do not have a need to convert the other way, this will not currently be implemented, however if enough people wish for this feature, I will do my best to create a version with that enabled. If you have any other ideas for features within the parser, please let me know so I can create them.
+
+## Issues
+If there is a problem that you experience with the code, whether a get/set index error or an undesired set of values, please open an issue on GitHub describing the problem in detail so I can fix it ASAP.
--- yaml_parser.gd
+++ yaml_parser.gd
@@ -1,278 +1,157 @@
 class_name YAMLParser
 extends Resource
 
-# Class for a parser that turns YAML "code" into a dictionary to use in Godot
-# The number of indents equals the level an item is at in the dictionary
-# 
-# Please NOTE: This will only save items in a key : value pair, so functions
-# will have to be parsed from its text to function form
+## Class for a parser that turns YAML code into a dictionary type to be used
+## within Godot Engine
+## All code written by Paperzlel
 
 
-# Filepath to where the YAML folder is. Modify it to where you want to read
-# files from. 
+## Filepath to the folder where the YAML files are stored. The code expects to
+## only deal with a single folder, sub-folders and files outside of the folder
+## WILL NOT be accounted for, you will have to change the filepath manually.
 var filepath : Variant = "res://%s.yaml"
 
-# Dictionary that holds all of the YAML information
+## The dictionary that holds all of the parsed values to be used elsewhere. The 
+## parser will only fill this up when the method `get_and_parse_yaml_file` is
+## called. 
 var yaml_dict : Dictionary = { }
 
-# String for the last key
-var current_region : String = ""
-
-# The key before the current one, used to compare whether to start a new dictionary
-# or not.
-var previous_key : String = ""
-
-# Boolean to check if the file has read the first line or not.
-var has_read_first_line : bool = false
-
-
-# Class constructor. Add your new filepath (path/to/file/) here.
+## Class constructor for the parser.
 func _init(n_filepath : String):
 	filepath = "res://" + n_filepath + "%s.yaml"
 
 
-# Checks if the given line has an indentation in it.
-# Returns false if no, true if yes.
-func has_indent(line : String) -> bool:
-	var line2 = line
-	if line2.dedent() == line:
-		return false
-	return true
+## The main function one will use. The parameter `name` will use the given
+## filepath combined with a given name.
+func get_and_parse_yaml_file(name : String) -> Dictionary:
+	# The path to the file to read
+	var dialogue_filepath : String = filepath % name
+	
+	# If the file doesn't exist return a blank dictionary and print an error.
+	if not FileAccess.file_exists(dialogue_filepath):
+		printerr("File does not exist and the parser is unable to parse, returning a blank dictionary. Please give a valid name for the file.")
+		return { }
+	
+	var file = FileAccess.open(dialogue_filepath, FileAccess.READ)
+	
+	## Temporary dictionary to store all of the items in as the parser goes along
+	var dict : Dictionary = { }
+	## Current "depth" into the file, or the number of indentations on a line
+	var index = 0
+	## Dictionary full of arrays of the lines at a given index, pre-parsed
+	var lines_at_index = { }
+	
+	# Iterate over every line in a file until the EOF is reached
+	while file.get_position() < file.get_length():
+		## Read the given line
+		var line : Dictionary = _return_line_key_and_value(file)
+		
+		# Create an object of the line's key and value
+		var object : Dictionary= {"key": line["key"], "value": line["value"]}
+		
+		# Check if line at the given index exists so as to not overwrite it
+		if lines_at_index.has(line["index"]):
+			lines_at_index[line["index"]].append(object)
+		else:
+			lines_at_index[line["index"]] = Array()
+			lines_at_index[line["index"]].append(object)
+	
+	# Set the dictionary to be formatted into the desired type
+	dict = _format_dict_from_other_r(dict, lines_at_index, index)
+	
+	# Set the public dictionary to be the same as the private one
+	yaml_dict = dict
+	# Return the formatted dictionary as given
+	return dict
 
 
-# Checks a line and returns the number of indents in that line. 0 is none.
-func get_indent_count(line : String) -> int:
+## Returns the number of indents in a line
+func _get_indent_count(line : String) -> int:
 	var line2 = line.dedent()
 	if line2 == line:
 		return 0
 	return len(line) - len(line2)
 
 
-# Function to separate the string into its key and value pair.
-func parse_key_and_value(line : String) -> PackedStringArray:
+## Separates out a line into its key and value.
+func _parse_key_and_value(line : String) -> PackedStringArray:
 	# Array 0 = key, 1 = value
 	var line_array = line.split(":", true)
 	return line_array
 
 
-# Finds the filepath for the given YAML file and parses it into a Dictionary
-func get_and_parse_yaml_file(name : String) -> Dictionary:
-	# creates a path using a name. This is usually because we expect more than
-	# one file to be used from a given location, so go crazy.
-	var dialogue_filepath : String = filepath % name
-	# If the file doesn't exist we return nothing. Customise if you have any
-	# default options you want.
-	if not FileAccess.file_exists(dialogue_filepath):
+## Calculates several values a given line will have for use later on in the pipeline
+func _return_line_key_and_value(file) -> Dictionary:
+	# Get the current line to read from the file
+	var line = file.get_line()
+	# Get the indent count from the given line (no. of tab spaces)
+	var index = _get_indent_count(line)
+	# Check for if the file is just the null terminator
+	if len(line) == 0: 
 		return { }
-	var file = FileAccess.open(dialogue_filepath, FileAccess.READ)
-	
-	# Interal variable for the "keys" in a path. This is effectively what we
-	# use to translate YAML files to dictionaries, as instead of indent paths
-	# we use dictionary keys.
-	var current_keys : Array = []
-
-	# Main file loop.
-	while file.get_position() < file.get_length():
-		var line = file.get_line()
-		var has_value : bool = false
-		
-		# If the line has a # key at the start, it is a comment and we skip this
-		# iteration of the loop.
-		if line.begins_with("#"):
-			continue
-
-		# Regex string that checks if the string has a colon followed by any letters
-		# If there is, the line has a value, if not, it doesn't.
-		var regex = RegEx.new()
-		regex.compile(".+:.+[a-zA-Z0-9]")
-		if regex.search(line):
-			has_value = true
-		
-		# Nested loop, as we want to update our keys per line, which we can't do 
-		# otherwise to my knowledge.
-		current_keys = add_contents_to_dict(line, current_keys, has_value)
-		
-	return yaml_dict
-
-
-# Adds the current key-value pair to the dictionary. Returns an array of the
-# next iteration of keys to use in the next line.
-func add_contents_to_dict(line : String, keys : Array, has_value : bool) -> Array:
-	# Copy the dictionary used in the previous line.
-	var dict = yaml_dict
-	
-	var indent_count = get_indent_count(line)
-	var keys_size = keys.size()
-	var line_array = parse_key_and_value(line)
-	
+	# Check if the line is a comment or has a comment
+	if line.begins_with("#"):
+		return { }
+	line = line.split("#")[0]
+	# Parse out the key and value, and set them as their own variables
+	var line_array = _parse_key_and_value(line)
 	var key = line_array[0].dedent()
 	var value
-	
-	# Logic for indent count and keys comparison. Used to change the number of keys
-	# and therefore the index level of the keys.
-	if indent_count == 0 and keys_size > 0:
-		keys.clear()
-		keys.append(key)
-		current_region = key
-	
-	elif keys_size == indent_count:
-		keys.append(key)
-		if not has_read_first_line:
-			current_region = key
-			has_read_first_line = true
-	
-	elif keys_size == indent_count + 1:
-		var n_key = keys.pop_back()
+	if line_array[1] == "":
+		value = null
+	else:
+		value = line_array[1].strip_edges()
+	# Return with all the values set NOTE: has_value CAN be removed, but for
+	# the meantime I will keep it
+	return {"index": index, "line_array": line_array, "key": key, "value": value}
 
-		if n_key == key:
-			keys.push_back(n_key)
+
+## Recursive formatting method, adds all the relevant items into a dictionary.
+func _format_dict_from_other_r(end_dict : Dictionary, indexed_dict : Dictionary,  \
+		index : int) -> Dictionary:
+	# Check the index is not larger the the size of the dictionary
+	if index + 1 > indexed_dict.size():
+		printerr("The index is greater than the size of the indexed dictionary!")
+		return { }
+	# Loop through every item at the given index
+	for item in indexed_dict[index]:
+		# Check if item's value is null and the item's name does not yet exist
+		if (item["value"] == null or item["value"] == "") and \
+				not end_dict.has(item["key"]):
+			end_dict[item["key"]] = Dictionary()
+			end_dict[item["key"]] = _format_dict_from_other_r(end_dict[item["key"]],  \
+					indexed_dict, index + 1)
+		# Check if item's value is null and the name exists, but is not an array
+		elif (item["value"] == null or item["value"] == "") and \
+				typeof(end_dict[item["key"]]) != TYPE_ARRAY:
+			var saved_item = end_dict[item["key"]]
+			end_dict[item["key"]] = Array()
+			end_dict[item["key"]].append(saved_item)
+			end_dict[item["key"]].append(_format_dict_from_other_r(end_dict[item["key"]], \
+					indexed_dict, index + 1))
+		# Check if item's value is null and the name exists and is an array
+		elif (item["value"] == null or item["value"] == "") and \
+				typeof(end_dict[item["key"]]) == TYPE_ARRAY:
+			var last_index = end_dict[item["key"]].size()
+			end_dict[item["key"]].append(Dictionary())
+			end_dict[item["key"]][last_index] = _format_dict_from_other_r( \
+					end_dict[item["key"]][last_index], indexed_dict, index + 1)
+		# Item's value is not null
 		else:
-			keys.push_back(key)
-			
-	elif keys_size > indent_count + 1:
-		while keys_size > indent_count:
-			keys.pop_back()
-			keys_size = keys.size()
-		keys.push_back(key)
-	
-	# Check if the line has a value, then set value and remove whitespace
-	if has_value and len(line_array) > 1:
-		value = line_array[1]
-		value = value.right(-1)
-	
-	# Core match for keys. Currently very repetitive and I'm not sure on a way to avoid that (yet)
-	# Anyone who wants to find a way to do this better is welcome to open an issue on GitHub and
-	# let me know.
-	match indent_count:
-		0:
-			if has_value:
-				dict[key] = value
+			# Item's name exists, but is not an array
+			if end_dict.has(item["key"]) and \
+					typeof(end_dict[item["key"]]) != TYPE_ARRAY:
+				var saved_item = end_dict[item["key"]]
+				end_dict[item["key"]] = Array()
+				end_dict[item["key"]].append(saved_item)
+				end_dict[item["key"]].append(item["value"])
+			# Item's name exists, and is an array
+			elif end_dict.has(item["key"]) and \
+					typeof(end_dict[item["key"]]) == TYPE_ARRAY:
+				end_dict[item["key"]].append(item["value"])
+			# Item's name does not exist so we can safely assign the item
 			else:
-				dict[key] = Dictionary()
-		1:
-			# Check for if the key has a value or not
-			if has_value:
-				# Check if the keys are the same. Two of the same key will override one another, so
-				# we copy its value and create an array for the keys.
-				if previous_key == key:
-					# Check if the keys do not hold an array
-					if typeof(dict[keys[0]][key]) != TYPE_ARRAY:
-						# Copy the key that already exists
-						var value_copy = dict[keys[0]][key]
-						# Convert it to an empty array
-						dict[keys[0]][key] = Array()
-						# Append the copied value to the array first (parser assumes the previous
-						# key was the first intended entry)
-						dict[keys[0]][key].append(value_copy)
-						# Append the desired value
-						dict[keys[0]][key].append(value)
-					else:
-						# Key is already an array, simply append the value to it
-						dict[keys[0]][key].append(value)
-				else:
-					# Key is not the same as the previous, just add its value as a String
-					dict[keys[0]][key] = value
-			# Key does not have a value
-			else:
-				# Key has the same name as the previous key, but doesn't have a value, so we append
-				# null to the array. DO NOT DO THIS (for now). It will mess up the code, and having
-				# a dictionary as an array index is not currently supported. 
-				if previous_key == key:
-					if typeof(dict[keys[0]][key]) != TYPE_ARRAY:
-						var value_copy = dict[keys[0]][key]
-						dict[keys[0]][key] = Array()
-						dict[keys[0]][key].append(value_copy)
-						dict[keys[0]][key].append(value)
-				else:
-					# Key is not the same as before, create a new dictionary for
-					# it to use instead
-					dict[keys[0]][key] = Dictionary()
-		# Code from this point is essentially the same as before, just with an extra keys index for it to
-		# go through. 
-		2:
-			if has_value:
-				if previous_key == key:
-					if typeof(dict[keys[0]][keys[1]][key]) != TYPE_ARRAY:
-						var value_copy = dict[keys[0]][keys[1]][key]
-						dict[keys[0]][keys[1]][key] = Array()
-						dict[keys[0]][keys[1]][key].append(value_copy)
-						dict[keys[0]][keys[1]][key].append(value)
-					else:
-						dict[keys[0]][keys[1]][key].append(value)
-				else:
-					dict[keys[0]][keys[1]][key] = value
-			else:
-				if previous_key == key:
-					if typeof(dict[keys[0]][keys[1]][key]) != TYPE_ARRAY:
-						var value_copy = dict[keys[0]][keys[1]][key]
-						dict[keys[0]][keys[1]][key] = Array()
-						dict[keys[0]][keys[1]][key].append(value_copy)
-						dict[keys[0]][keys[1]][key].append(value)
-				else:
-					dict[keys[0]][keys[1]][key] = Dictionary()
-		3:
-			if has_value:
-				if previous_key == key:
-					if typeof(dict[keys[0]][keys[1]][keys[2]][key]) != TYPE_ARRAY:
-						var value_copy = dict[keys[0]][keys[1]][keys[2]][key]
-						dict[keys[0]][keys[1]][keys[2]][key] = Array()
-						dict[keys[0]][keys[1]][keys[2]][key].append(value_copy)
-						dict[keys[0]][keys[1]][keys[2]][key].append(value)
-					else:
-						dict[keys[0]][keys[1]][keys[2]][key].append(value)
-				else:
-					dict[keys[0]][keys[1]][keys[2]][key] = value
-			else:
-				if previous_key == key:
-					if typeof(dict[keys[0]][keys[1]][keys[2]][key]) != TYPE_ARRAY:
-						var value_copy = dict[keys[0]][keys[1]][keys[2]][key]
-						dict[keys[0]][keys[1]][keys[2]][key] = Array()
-						dict[keys[0]][keys[1]][keys[2]][key].append(value_copy)
-						dict[keys[0]][keys[1]][keys[2]][key].append(value)
-				else:
-					dict[keys[0]][keys[1]][keys[2]][key] = Dictionary()
-		4:
-			if has_value:
-				if previous_key == key:
-					if typeof(dict[keys[0]][keys[1]][keys[2]][keys[3]][key]) != TYPE_ARRAY:
-						var value_copy = dict[keys[0]][keys[1]][keys[2]][keys[3]][key]
-						dict[keys[0]][keys[1]][keys[2]][keys[3]][key] = Array()
-						dict[keys[0]][keys[1]][keys[2]][keys[3]][key].append(value_copy)
-						dict[keys[0]][keys[1]][keys[2]][keys[3]][key].append(value)
-					else:
-						dict[keys[0]][keys[1]][keys[2]][keys[3]][key].append(value)
-				else:
-					dict[keys[0]][keys[1]][keys[2]][keys[3]][key] = value
-			else:
-				if previous_key == key:
-					if typeof(dict[keys[0]][keys[1]][keys[2]][keys[3]][key]) != TYPE_ARRAY:
-						var value_copy = dict[keys[0]][keys[1]][keys[2]][keys[3]][key]
-						dict[keys[0]][keys[1]][keys[2]][keys[3]][key] = Array()
-						dict[keys[0]][keys[1]][keys[2]][keys[3]][key].append(value_copy)
-						dict[keys[0]][keys[1]][keys[2]][keys[3]][key].append(value)
-				else:
-					dict[keys[0]][keys[1]][keys[2]][keys[3]][key] = Dictionary()
-		5:
-			if has_value:
-				if previous_key == key:
-					if typeof(dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][key]) != TYPE_ARRAY:
-						var value_copy = dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][key]
-						dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][key] = Array()
-						dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][key].append(value_copy)
-						dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][key].append(value)
-					else:
-						dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][key].append(value)
-				else:
-					dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][key] = value
-			else:
-				if previous_key == key:
-					if typeof(dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][key]) != TYPE_ARRAY:
-						var value_copy = dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][key]
-						dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][key] = Array()
-						dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][key].append(value_copy)
-						dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][key].append(value)
-				else:
-					dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][key] = Dictionary()
-
-	previous_key = key
-	return keys
+				end_dict[item["key"]] = item["value"]
+	# Return once all lines are configured, recusion means deeper dictionaries
+	# will return the same way as the main one
+	return end_dict
